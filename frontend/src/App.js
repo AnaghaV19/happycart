ad2494efcf58bafdc9babafad11b8bcddfe817b8
AnaghaV19 <anaghav.athreya@gmail.com>
2022-05-17 14:47:27 +0530

    Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div >
+      <header >
+        <a href="/" >Happy Cart</a>
+      </header>
+      <main>
+        Products List
+      </main>
+    </div>
+  );
+}
+
+export default App;
