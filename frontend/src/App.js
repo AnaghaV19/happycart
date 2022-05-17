9d906b0923329925a42f54812144d78c91aba042
AnaghaV19 <anaghav.athreya@gmail.com>
2022-05-17 16:13:36 +0530

    products list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,29 @@
-
+import data from './data';
 
 function App() {
   return (
-    <div >
-      <header >
-        <a href="/" >Happy Cart</a>
+    <div>
+      <header>
+        <a href="/">Happy Cart</a>
       </header>
       <main>
-        Products List
+        <h1> Available products</h1>
+        <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+            <img src={product.image} alt={product.name} />
+            </a>
+            <div className="productinfo">
+            <a href={`/product/${product.slug}`}>
+            <p>{product.name}</p>
+            </a>
+            <p><strong>Rs{product.price}</strong></p>
+            <button>Add to cart</button>
+            </div>
+          </div >
+        ))}
+        </div>
       </main>
     </div>
   );
