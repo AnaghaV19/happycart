33ebcce09cfb0826f994e02683bd7780ff477ce1
AnaghaV19 <anaghav.athreya@gmail.com>
2022-05-23 22:15:53 +0530

    deleted profile part

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -45,9 +45,7 @@ function App() {
                 </Link>
                 {userInfo ? (
                   <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
-                    <LinkContainer to="/profile">
-                      <NavDropdown.Item>User Profile</NavDropdown.Item>
-                    </LinkContainer>
+                   
                     <LinkContainer to="/orderhistory">
                       <NavDropdown.Item>Order History</NavDropdown.Item>
                     </LinkContainer>
